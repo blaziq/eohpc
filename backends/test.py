a8cbe538a2a6f610c234c77353993327287be1dd
Blazej Fitrzyk <blazej.fitrzyk@solenix.ch>
2026-02-12 16:51:22 +0100

    f

--- backends/test.py
+++ backends/test.py
@@ -5,7 +5,7 @@ from typing import Any, Dict
 
 from hpc_submit import BaseConfig, BaseBackend, ConfigError  # import from main module
 
-@dataclass(frozen=True)
+@dataclass
 class TestConfig(BaseConfig):
     a: str
 
