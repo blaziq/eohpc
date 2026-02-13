d69207007407e37bccd71a8ff25f3608f01d26d5
Blazej Fitrzyk <blazej.fitrzyk@solenix.ch>
2026-02-13 12:50:53 +0100

    1

--- hpc_submit.sh
+++ hpc_submit.sh
@@ -10,9 +10,3 @@ OUTDIR="$(python3 $SCRIPT_DIR/hpc_submit.py "$MODE" "$@")"
 echo "Generated in: $OUTDIR"
 
 exec ${OUTDIR}/${MODE}_submit.sh
-
-#if [ "$MODE" = "spacehpc" ]; then
-#  exec "$OUTDIR/submit_spacehpc.sh"
-#else
-#  exec condor_submit "$OUTDIR/job.sub"
-#fi
